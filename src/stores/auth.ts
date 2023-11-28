3f837b5a5b857a2bd57c767797d0a7c3a0ec0643
Marc Bäckman <marcbackman.lapinamk@gmail.com>
2023-11-28 05:40:25 +0200

    started front authorization

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -21,6 +21,54 @@ const useAuthStore = create(persist<AuthStore>((set) => ({
     login: async (credentials: LoginCredentials) => {
 
       try {
+        const {csrf_token} = await loginService(credentials);
+        //const {csrf_token} = await loginService(new URLSearchParams(credentials))
+        if(csrf_token){
+          set({isAuth: true})
+        }
+      } catch(e) {
+        console.log("Error with login in authStore", e)
+        throw new Error("Login failed")
+      }
+
+    },
+    logout: () => {
+      return set(() => ({
+        isAuth: false
+      }))
+    }
+
+}), {
+    name: "auth-store", 
+    storage: createJSONStorage(() => sessionStorage)
+}));
+
+export default useAuthStore;
+
+/*
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import loginService from "../services/auth"
+
+
+interface LoginCredentials {
+  username: string;
+  password: string;
+}
+
+type AuthStore = {
+  isAuth: boolean;
+  //csrfToken: string | null;
+  login: (credentials: LoginCredentials) => Promise<void>;
+  logout: () => void;
+};
+
+const useAuthStore = create(persist<AuthStore>((set) => ({
+    isAuth: false,
+    //csrfToken: null,
+    login: async (credentials: LoginCredentials) => {
+
+      try {
         const {csrf_token} = await loginService(credentials)
         if(csrf_token){
           set({isAuth: true})
@@ -43,3 +91,4 @@ const useAuthStore = create(persist<AuthStore>((set) => ({
 }));
 
 export default useAuthStore;
+*/
--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-export default defineConfig({
-  plugins: [react()]
-})
 
-/*
-// NÄMÄ ASETUKSET SITTEN KUN SAADAAN DOCKER TOIMIMAAN
+// START WITH https://localhost:5173
 
 export default defineConfig({
   plugins: [react(), basicSsl()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://localhost:8001",
         secure: false,
         changeOrigin: true,
         //rewrite: (path) => path +
       },
-      "/docs": "http://localhost:8000",
-      "/openapi.json": "http://localhost:8000"
+      "/docs": "http://localhost:8001",
+      "/openapi.json": "http://localhost:8001"
     }
   }
 })
-*/
