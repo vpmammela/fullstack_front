f12e4e444f0722c61c8d7f566c7e23895e93de94
Marc Bäckman <marcbackman.lapinamk@gmail.com>
2023-11-28 05:44:35 +0200

    removed unnecessary code

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -22,7 +22,6 @@ const useAuthStore = create(persist<AuthStore>((set) => ({
 
       try {
         const {csrf_token} = await loginService(credentials);
-        //const {csrf_token} = await loginService(new URLSearchParams(credentials))
         if(csrf_token){
           set({isAuth: true})
         }
@@ -44,51 +43,3 @@ const useAuthStore = create(persist<AuthStore>((set) => ({
 }));
 
 export default useAuthStore;
-
-/*
-import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
-import loginService from "../services/auth"
-
-
-interface LoginCredentials {
-  username: string;
-  password: string;
-}
-
-type AuthStore = {
-  isAuth: boolean;
-  //csrfToken: string | null;
-  login: (credentials: LoginCredentials) => Promise<void>;
-  logout: () => void;
-};
-
-const useAuthStore = create(persist<AuthStore>((set) => ({
-    isAuth: false,
-    //csrfToken: null,
-    login: async (credentials: LoginCredentials) => {
-
-      try {
-        const {csrf_token} = await loginService(credentials)
-        if(csrf_token){
-          set({isAuth: true})
-        }
-      } catch(e) {
-        console.log("Error with login in authStore", e)
-        throw new Error("Login failed")
-      }
-
-    },
-    logout: () => {
-      return set(() => ({
-        isAuth: false
-      }))
-    }
-
-}), {
-    name: "auth-store", 
-    storage: createJSONStorage(() => sessionStorage)
-}));
-
-export default useAuthStore;
-*/
