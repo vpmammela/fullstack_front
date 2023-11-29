9fd5cec9260344eb06326f2c452c35960f3a9b84
Marc Bäckman <marcbackman.lapinamk@gmail.com>
2023-11-29 17:58:14 +0200

    works with docker

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ import { NotificationProvider } from './NotificationContext.tsx'
 
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
-//axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true
 
 
 const router = createBrowserRouter([
