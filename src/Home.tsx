ec372a578f9a329037283c460df5c3635bd6f284
MirkaHienonen <mhienone@edu.lapinamk.fi>
2023-11-30 18:45:32 +0200

    Sliding panel moved /home, some styling added

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,29 +1,82 @@
-
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './components/styles.css'
-  
+import styled from "styled-components";
+import SlidingPanel from "./Instructions";
+import logo from './Images/logo.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: lightgray;
+  min-height: 100vh;
+`;
+
+const OpenInsctructions = styled.div`
+  position: fixed;
+  top: 10px;
+  left: 10px;
+  cursor: pointer;
+  z-index: 2; 
+`;
+
+const LinkButton = styled.div`
+  /* Add styles*/
+`;
+
+const ReviewLink = styled(Link)`
+  /* Add styles*/
+`;
+
+const LogoContainer = styled.div`
+  margin-top: 10%;
+`;
+
+const LogoImage = styled.img`
+  width: 300px;
+  height: auto;
+`;
+
 const Home = () => {
+  const [isPanelOpen, setIsPanelOpen] = useState(false); // Instructions closed by default.
+
+  // Open isntructions.
+  const togglePanel = () => {
+    setIsPanelOpen(!isPanelOpen);
+  };
+
   return (
-    <div className="home-container">
-      <div className='selection-background'>
-        <div className="linkbutton">
-          <Link className="review-link" to="/roomselection">
-            Tee katselmointi
-          </Link>
-        </div>
-        <div className="linkbutton">
-          <Link className="review-link" to="/roomselection">
-            Huoneen ohjeet
-          </Link>
-        </div>
-        <div className="linkbutton">
-          <Link className="review-link" to="/roomselection">
-            Raportit
-          </Link>
-        </div>
-      </div>
-    </div>
-  )
-}
+    <HomeContainer>
+      <OpenInsctructions>
+        <FontAwesomeIcon
+          icon={faArrowCircleLeft}
+          onClick={togglePanel}
+          className="arrow-icon"
+        />
+      </OpenInsctructions>
+      <SlidingPanel isOpen={isPanelOpen} togglePanel={togglePanel} />
+      <LogoContainer>
+        <LogoImage src={logo} alt="Logo" />
+      </LogoContainer>
+      <LinkButton>
+        <ReviewLink to="/roomselection" className="review-link">
+          Tee katselmointi
+        </ReviewLink>
+      </LinkButton>
+      <LinkButton>
+        <ReviewLink to="/roomselection" className="review-link">
+          Huoneen ohjeet
+        </ReviewLink>
+      </LinkButton>
+      <LinkButton>
+        <ReviewLink to="/roomselection" className="review-link">
+          Raportit
+        </ReviewLink>
+      </LinkButton>
+    </HomeContainer>
+  );
+};
 
 export default Home;
