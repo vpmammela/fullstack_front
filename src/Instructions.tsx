bf46e75c7ac7fe82d40a2a2b40fde11bca7c7be3
MirkaHienonen <mhienone@edu.lapinamk.fi>
2023-11-30 15:10:59 +0200

    styled components added, instruction links added

--- src/Instructions.tsx
+++ src/Instructions.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import './SlidingPanel.css';
 
 interface InstructionsProps {
@@ -6,23 +7,48 @@ interface InstructionsProps {
   togglePanel: () => void;
 }
 
-// Sliding panel to show instructions to how to use app. 
+// Styled component for bold text
+const BoldText = styled.span`
+  font-weight: bold;
+`;
+
+// Sliding panel to show instructions on how to use the app.
 const Instructions: React.FC<InstructionsProps> = ({ isOpen, togglePanel }) => {
   return (
-    // Close panel if open. 
+    // Close panel if open.
     <div className={`sliding-panel ${isOpen ? 'open' : ''}`}>
       <button onClick={togglePanel} className="toggle-button">
         Close instructions
       </button>
       <div className="content">
-        <h2>Ohjeistus</h2>
-        {/* TODO: instructions go here */}
-        <p>"Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        <h2>Review instructions</h2>
+        <br />
+        {/* External links with shortened text. */}
+        <p>
+          <BoldText>
+            <a href="https://lucit.sharepoint.com/:w:/r/sites/pilvi/_layouts/15/Doc.aspx?sourcedoc=%7B25E73C81-28CC-4911-9843-024E9E039061%7D&file=6S%20jatkuvat%20katselmoinnit.docx&action=default&mobileredirect=true" target="_blank" rel="noopener noreferrer">
+              Continuous reviews
+            </a>
+          </BoldText>
+          <br /><br />
+          <BoldText>
+            <a href="https://lucit.sharepoint.com/:w:/r/sites/pilvi/_layouts/15/Doc.aspx?sourcedoc=%7B891C1F7F-2E0F-425F-B759-AD175B340F3D%7D&file=6S%20lukukausi-%20ja%20vuosikatselmoinnit.docx&action=default&mobileredirect=true" target="_blank" rel="noopener noreferrer">
+              Semester reviews
+            </a>
+          </BoldText>
+          <br /><br />
+          <BoldText>
+            <a href="https://lucit.sharepoint.com/:w:/r/sites/pilvi/_layouts/15/Doc.aspx?sourcedoc=%7B811C930B-AE8F-4F9B-9082-A0492F5C0894%7D&file=6S%20toimintamallin%20ja%20johtamisen%20katselmoinnit.docx&action=default&mobileredirect=true" target="_blank" rel="noopener noreferrer">
+              Management reviews
+            </a>
+          </BoldText>
+          <br /><br />
+          <BoldText>
+            <a href="https://lucit.sharepoint.com/:w:/r/sites/pilvi/_layouts/15/Doc.aspx?sourcedoc=%7BC7667C66-7BC4-48DC-9F85-01C6F06065DA%7D&file=6S%20turvallisuuskatselmoinnit.docx&action=default&mobileredirect=true" target="_blank" rel="noopener noreferrer">
+              Safety reviews
+            </a>
+          </BoldText>
+        </p>
       </div>
     </div>
   );
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,79 +1,168 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import { useUser } from '../../UserContext';
-import useAuthStore from "../../stores/auth";
-import { useNotification } from "../../NotificationContext";
-import { useNavigate } from "react-router-dom";
-import Notification from "../Notification/Notification";
+import useAuthStore from '../../stores/auth';
+import { useNotification } from '../../NotificationContext';
+import { useNavigate } from 'react-router-dom';
+import Notification from '../Notification/Notification';
 import redSnow from '../../Images/redsnow.jpg';
+import logo from '../../Images/logo.png';
+
+const AppContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-image: url(${redSnow});
+  background-size: cover;
+  background-position: center;
+  overflow: hidden; /* Scrolling disabled */
+`;
+
+// AMK logo.
+const LogoContainer = styled.div`
+  position: absolute;
+  top: 10%;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 2; /* Logo is on top of everything */
+`;
+
+const LogoImage = styled.img`
+  width: 300px;
+  height: auto;
+`;
+
+// Background for loginform.
+const GrayHalfBall = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 60vh;
+  background-color: lightgray;
+  border-top-left-radius: 40% 50px;
+  border-top-right-radius: 40% 50px;
+  overflow: hidden;
+`;
+
+// Login form position.  
+const LoginFormContainer = styled.div`
+  position: absolute;
+  top: 60%; /* Adjust the top position */
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  z-index: 1; /* Login form is on top of the gray half ball */
+`;
+
+// Content of login form. 
+const LoginFormContent = styled.div`
+  width: 100%;
+  max-width: 400px;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
+// Input fields.
+const StyledInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+  border: none;
+  border-radius: 5px;
+  background-color: white; /* White background for the form fields */
+`;
+
+// Login button.
+const StyledButton = styled.button`
+  width: 100%;
+  padding: 12px;
+  box-sizing: border-box;
+  border: none;
+  border-radius: 5px;
+  background-color: #C9431B; /* Custom color for the button */
+  color: white; /* Text color for the button */
+  cursor: pointer;
+`;
+
+const SignInHeader = styled.h2`
+  color: red;
+  margin-top: 0;
+  margin-left: 38%
+`;
 
 const LoginForm = () => {
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   const { setUser } = useUser();
   const { setNotification } = useNotification();
   const authStore = useAuthStore();
   const navigate = useNavigate();
 
-  const handleLogin = async (event: { preventDefault: () => void; }) => {
+  const handleLogin = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
 
     try {
-      await authStore.login({username, password});
+      await authStore.login({ username, password });
 
       const user = {
         username,
-        role: "user role" // value can be student, staff, or admin
+        role: 'user role', // value can be student, staff, or admin
       };
-      window.sessionStorage.setItem("loggedUser", JSON.stringify(user));
+      window.sessionStorage.setItem('loggedUser', JSON.stringify(user));
 
-      setUsername("");
-      setPassword("");
-      setUser(user.username)
+      setUsername('');
+      setPassword('');
+      setUser(user.username);
 
-      setNotification(`logging in with ${user.username}`);
-      navigate("/")
+      setNotification(`Logging in with ${user.username}`);
+      navigate('/');
     } catch (exception) {
-      setNotification("Tarkista kirjautumistiedot");
+      setNotification('Tarkista kirjautumistiedot');
     }
   };
 
   return (
-    <div className="app-container">
-      <Notification/>
-      <div className="login-form-container">
-        <div className="logincontainer">
-          <div className="form">
-        <h2 className="login-title">Sign in</h2>
-        
-        <form onSubmit={handleLogin}>
-            <div className="usernametext">username</div>
-            <input
+    <AppContainer>
+      <Notification />
+      <LogoContainer>
+        <LogoImage src={logo} alt="Logo" />
+      </LogoContainer>
+      <GrayHalfBall />
+      <LoginFormContainer>
+        <LoginFormContent>
+          <SignInHeader>Sign in</SignInHeader>
+          <form onSubmit={handleLogin}>
+            <StyledInput
               id="username"
               type="text"
               value={username}
               name="Username"
               onChange={({ target }) => setUsername(target.value)}
+              placeholder="Username"
             />
-            <div className="passwordtext">password</div>
-            <input
+            <StyledInput
               id="password"
               type="password"
               value={password}
               name="Password"
               onChange={({ target }) => setPassword(target.value)}
+              placeholder="Password"
             />
-          <button id="login-button" type="submit" className="loginbutton">
-            Login
-          </button>
-        </form>
-        </div>
-      </div>
-      </div>
-
-      <div className="background-container">
-        <img src={redSnow} alt="Background" className="background-image" /> 
-      </div>
-    </div>
+            <StyledButton id="login-button" type="submit">
+              Login
+            </StyledButton>
+          </form>
+        </LoginFormContent>
+      </LoginFormContainer>
+    </AppContainer>
   );
 };
 
