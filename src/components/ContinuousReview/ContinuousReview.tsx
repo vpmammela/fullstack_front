be293f17fb0b09fb7a4272eb2285e4776d8a8356
MirkaHienonen <mhienone@edu.lapinamk.fi>
2023-12-01 20:55:26 +0200

    Able to add photos to forms

--- src/components/ContinuousReview/ContinuousReview.tsx
+++ src/components/ContinuousReview/ContinuousReview.tsx
@@ -1,71 +1,42 @@
-import "../styles.css";
-import { Form} from "react-router-dom";
+import React, { useState, ChangeEvent } from 'react';
+import { Form } from 'react-router-dom';
 import { useUser } from '../../UserContext';
 
-
-//ACTION
-export async function action({request}: { request: Request }){
-    const formData = await request.formData()
-    //data variable is an object containing the key-value pairs from the form that were previously collected into the formData object.
-    const data = Object.fromEntries(formData)
-    const response = await fetch("",{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json"
-      },
-      body:JSON.stringify(data)
-    })
-    //Onko hyvä nimi result
-    //mihin result pitäisi laittaa?
-    const result =await response.json()
-    console.log(result)
+// ACTION
+export async function action({ request }: { request: Request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const response = await fetch('', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  const result = await response.json();
+  console.log(result);
 }
 
-export default function ContinuousReview() {
+// ContinuousReview Component
+const ContinuousReview = () => {
   const { user } = useUser();
+  const [photo, setPhoto] = useState<File | null>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    setPhoto(file || null);
+  };
 
   return (
     <div>
-      <h2>
-        Jatkuva katselmointi
-      </h2>
+      <h2>Jatkuva katselmointi</h2>
       <p>Hei {user}, täytä katselmointi ja tallenna lopuksi</p>
       <Form method="post">
-        <label> Tilan yleisilme</label>
-        <div>
-          <span className="material-symbols-outlined">
-            sentiment_dissatisfied
-          </span>
-          <input type="radio" value="bad" name="condition"/>
-        </div>
-        <div>
-          <span className="material-symbols-outlined">
-            sentiment_satisfied
-          </span>
-          <input type="radio" value="good" name="condition"/>
-        </div>
-        <div>
-          <span className="material-symbols-outlined">
-            sentiment_very_satisfied
-          </span>
-          <input type="radio" value="excellent" name="condition"/>
-        </div>
-        <div>
-          <label>Huomiot
-          <input type="text" name="attentions"></input>
-          </label>
-        </div>
-        <div>
-          <label>Muita huomioita/ kehitysideoita
-          <input type="text" name="developmentIdeas"></input>
-          </label>
-        </div>
-        <div>
-          <label>Mitä positiivista olet huomannut tarkastusjasolla?
-          <input type="text" name="positiveThings"></input>
-          </label>
-        </div>
-        <label>**Tähän vielä valokuvan lisäys**</label>
+        <label>
+          Valitse kuva
+          {/*Add picture*/}
+          <input type="file" accept="image/*" capture="environment" onChange={handleFileChange} />
+        </label>
         <div>
           <button type="submit">Tallenna</button>
         </div>
@@ -73,4 +44,6 @@ export default function ContinuousReview() {
     </div>
   );
 };
+
+export default ContinuousReview;
   
