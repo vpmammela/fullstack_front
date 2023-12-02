7fac9b27f5606161a897ac08b6886e28ad20fee3
MirkaHienonen <mhienone@edu.lapinamk.fi>
2023-12-02 19:03:50 +0200

    Appearance improved

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,33 +5,55 @@ import SlidingPanel from "./Instructions";
 import logo from './Images/logo.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import redSnow from './Images/redsnow.jpg';
 
 const HomeContainer = styled.div`
+position: relative;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+height: 100vh;
+background-image: url(${redSnow});
+background-size: cover;
+background-position: center;
+overflow: hidden; /* Scrolling disabled */
+`;
+
+// Background for form.
+const GrayBackground = styled.div`
+  position: absolute;
+  top: 66%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 70vh;
+  background-color: lightgray;
+  border-top-left-radius: 0% 50px;
+  border-top-right-radius: 0% 50px;
+  overflow: hidden;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: lightgray;
-  min-height: 100vh;
-`;
-
-const OpenInsctructions = styled.div`
-  position: fixed;
-  top: 10px;
-  left: 10px;
-  cursor: pointer;
-  z-index: 2; 
+  box-sizing: border-box;
 `;
 
 const LinkButton = styled.div`
-  /* Add styles*/
+  padding-top: 20px;
+  margin-top: 2%;
 `;
 
 const ReviewLink = styled(Link)`
   /* Add styles*/
 `;
 
+// AMK logo.
 const LogoContainer = styled.div`
-  margin-top: 10%;
+  position: absolute;
+  top: 10%;
+  left: 50%;
+  transform: translateX(-50%); 
+  z-index: 2; /* Logo is on top of everything */
 `;
 
 const LogoImage = styled.img`
@@ -40,26 +62,13 @@ const LogoImage = styled.img`
 `;
 
 const Home = () => {
-  const [isPanelOpen, setIsPanelOpen] = useState(true); // Instructions closed by default.
-
-  // Open isntructions.
-  const togglePanel = () => {
-    setIsPanelOpen(!isPanelOpen);
-  };
-
+  
   return (
     <HomeContainer>
-      <OpenInsctructions>
-        <FontAwesomeIcon
-          icon={faArrowCircleLeft}
-          onClick={togglePanel}
-          className="arrow-icon"
-        />
-      </OpenInsctructions>
-      <SlidingPanel isOpen={isPanelOpen} togglePanel={togglePanel} />
       <LogoContainer>
         <LogoImage src={logo} alt="Logo" />
       </LogoContainer>
+      <GrayBackground>
       <LinkButton>
         <ReviewLink to="/roomselection" className="review-link">
           Tee katselmointi
@@ -75,6 +84,7 @@ const Home = () => {
           Raportit
         </ReviewLink>
       </LinkButton>
+      </GrayBackground>
     </HomeContainer>
   );
 };
--- src/Root.tsx
+++ src/Root.tsx
@@ -5,21 +5,51 @@ import { Outlet } from "react-router-dom";
 import redSnow from './Images/redsnow.jpg';
 import TestBackendConnection from "./components/TestBackendConnection/TestBackendConnection";
 import './components/styles.css';
+import SlidingPanel from "./Instructions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+const OpenInsctructions = styled.div`
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  cursor: pointer;
+  z-index: 1000; /* Should render above everyhting everywhere?
+  TODO: Make sure it does ^ */
+`;
 
 export default function Root() {
+  const [isPanelOpen, setIsPanelOpen] = useState(true); // Instructions closed by default.
 
-  return (  
-      <><div className="app-container">
-      <Notification />
-      <div style={{ justifyContent: "center" }}>
-        <SignedInUser />
+  // Open isntructions.
+  const togglePanel = () => {
+    setIsPanelOpen(!isPanelOpen);
+  };
+
+  return (
+    <>
+      <div className="app-container">
+        <SlidingPanel isOpen={isPanelOpen} togglePanel={togglePanel} />
+        <OpenInsctructions>
+          <FontAwesomeIcon
+            icon={faArrowCircleLeft}
+            onClick={togglePanel}
+            className="arrow-icon"
+          />
+        </OpenInsctructions>
+        <Notification />
+        <div style={{ justifyContent: "center" }}>
+          <SignedInUser />
+        </div>
+        <div className="content-container">
+          <Outlet></Outlet>
+          <TestBackendConnection />
+        </div>
       </div>
-      <div className="content-container">
-        <Outlet></Outlet>
-        <TestBackendConnection />
-      </div>
-    </div><div className="background-container">
+      <div className="background-container">
         <img src={redSnow} alt="Background" className="background-image" />
-      </div></>
+      </div>
+    </>
   );
 }
