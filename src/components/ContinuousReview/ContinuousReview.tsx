38dd06b5207e2392456a78f362491d03171df607
Marc Bäckman <marcbackman.lapinamk@gmail.com>
2023-12-02 04:19:55 +0200

    small fix

--- src/components/ContinuousReview/ContinuousReview.tsx
+++ src/components/ContinuousReview/ContinuousReview.tsx
@@ -21,10 +21,19 @@ export async function action({ request }: { request: Request }) {
 const ContinuousReview = () => {
   const { user } = useUser();
   const [photo, setPhoto] = useState<File | null>(null);
+  const [photoUrl, setPhotoUrl] = useState<string | null>(null);
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     setPhoto(file || null);
+
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      console.log("image url:", imageUrl)
+      setPhotoUrl(imageUrl);
+    } else {
+      setPhotoUrl(null);
+    }
   };
 
   return (
@@ -32,11 +41,52 @@ const ContinuousReview = () => {
       <h2>Jatkuva katselmointi</h2>
       <p>Hei {user}, täytä katselmointi ja tallenna lopuksi</p>
       <Form method="post">
+        <label> Tilan yleisilme</label>
+        <div>
+          <span className="material-symbols-outlined">
+            sentiment_dissatisfied
+          </span>
+          <input type="radio" value="bad" name="condition"/>
+        </div>
+        <div>
+          <span className="material-symbols-outlined">
+            sentiment_satisfied
+          </span>
+          <input type="radio" value="good" name="condition"/>
+        </div>
+        <div>
+          <span className="material-symbols-outlined">
+            sentiment_very_satisfied
+          </span>
+          <input type="radio" value="excellent" name="condition"/>
+        </div>
+        <div>
+          <label>Huomiot
+          <input type="text" name="attentions"></input>
+          </label>
+        </div>
+        <div>
+          <label>Muita huomioita/ kehitysideoita
+          <input type="text" name="developmentIdeas"></input>
+          </label>
+        </div>
+        <div>
+          <label>Mitä positiivista olet huomannut tarkastusjasolla?
+          <input type="text" name="positiveThings"></input>
+          </label>
+        </div>
+        <br/>
         <label>
           Valitse kuva
           {/*Add picture*/}
           <input type="file" accept="image/*" capture="environment" onChange={handleFileChange} />
         </label>
+        <p>
+          Image preview:
+          {photoUrl && <img src={photoUrl} alt="Selected" style={{ maxWidth: '100%' }} />}
+        </p>
+        <div>
+      </div>
         <div>
           <button type="submit">Tallenna</button>
         </div>
