56fe1dee5e9cd19aa6786cea97f41e190661f6f8
vmammela <veli-pekka.mammela@edu.lapinamk.fi>
2023-12-03 11:48:25 +0200

    add locations to RoomSelection

--- src/datahandler/datahandler.tsx
+++ src/datahandler/datahandler.tsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 export const exampleFetch = async () => {
   try {
-    // const response = await axios.get('http://localhost:8001/api/v1/test');
-    const response = await axios.get('https://fullstack-backend-w94q.onrender.com/api/v1/test');
+    const response = await axios.get('http://localhost:8000/api/v1/test');
+    //const response = await axios.get('https://fullstack-backend-w94q.onrender.com/api/v1/test');
     return response.data;
   } catch (error) {
     throw error;
