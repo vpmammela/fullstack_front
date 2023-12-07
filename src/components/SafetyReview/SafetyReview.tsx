3204df7e60a019b1f583fc560c0a9f058bb6c4f5
MirkaHienonen <mhienone@edu.lapinamk.fi>
2023-12-07 18:02:46 +0200

    minor updates

--- src/components/SafetyReview/SafetyReview.tsx
+++ src/components/SafetyReview/SafetyReview.tsx
@@ -11,11 +11,10 @@ const SafetyReviewContainer = styled.div`
   height: 100vh;
   background-size: cover;
   background-position: center;
-  overflow: hidden; /* Scrolling disabled */
 `;
 
 const GrayBackground = styled.div`
-  position: fixed;
+  position: absolute;
   top: 66%;
   left: 50%;
   transform: translate(-50%, -50%);
@@ -30,6 +29,24 @@ const GrayBackground = styled.div`
   align-items: center;
   box-sizing: border-box;
 `;
+
+const FormContainer = styled.div`
+  position: absolute;
+  top: 100px; /* Adjust this value based on your Header height */
+  left: 50%;
+  transform: translate(-50%, 0);
+  width: 100%;
+  min-height: calc(100vh - 100px); /* Adjust this value based on your Header height */
+  background-color: lightgray;
+  border-top-left-radius: 0% 50px;
+  border-top-right-radius: 0% 50px;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-sizing: border-box;
+`;
+
 
 const PhotoInput = styled.input`
   margin-top: 5px;
@@ -46,13 +63,16 @@ const SafetyReview = () => {
 
   return (
     <SafetyReviewContainer>
-      <Header></Header>
+      <Header/>
+      <FormContainer>
       <GrayBackground>
     <h2>6S Turvallisuuskatselmointi</h2>
         <form>
             <div>
-              <label>Yleisilme</label>
+              <br></br>
+              <h4>Yleisilme</h4>
               <div>
+                <br></br>
               <label>Onko yleisilme siisi? Millä tasolla päivittäiskatselmointien tulokset ovat? Onko havaittuihin poikkeamiin reagoitu?</label>
               <div>
               <label>Puutteellinen</label>
@@ -70,7 +90,7 @@ const SafetyReview = () => {
           </div>
 
           <div>
-            <label>Turvallisuusohjeistukset</label>
+            <h4>Turvallisuusohjeistukset</h4>
             <div>
               <label>Onko ohjeistukset saatavilla ja ajantasaiset? Noudatetaanko annettuja ohjeita?</label>
               <div>
@@ -89,7 +109,7 @@ const SafetyReview = () => {
           </div>
 
           <div>
-            <label>Toiminnan riskiarviointi</label>
+            <h4>Toiminnan riskiarviointi</h4>
             <div>
               <label>Onko riskiarviointi ajantasalla ja onko toiminnan riskiä mahdollisuus pienentää?</label>
               <div>
@@ -108,7 +128,7 @@ const SafetyReview = () => {
           </div>
 
           <div>
-            <label>Suojavarusteet</label>
+            <h4>Suojavarusteet</h4>
             <div>
               <label>Onko henkilökohtaiset suojavarusteet tunnistettu ja saatavilla? Käytetäänkö niitä?</label>
               <div>
@@ -127,7 +147,7 @@ const SafetyReview = () => {
           </div>
 
           <div>
-            <label>Ensiapu- ja palosammuttimet</label>
+            <h4>Ensiapu- ja palosammuttimet</h4>
             <div>
               <label>Onko ensiaputarvikkeet ja palosammuttimet käytettävissä ja ajan tasalla?</label>
               <div>
@@ -146,7 +166,7 @@ const SafetyReview = () => {
           </div>
 
           <div>
-            <label>Kemikaalien hallinta</label>
+            <h4>Kemikaalien hallinta</h4>
             <div>
               <label>Onko kemikaaliluettelot ajan tasalla? Säilytetäänkö kemikaalit asianmukaisesti? Onko varoitusmerkit ja käyttöturvatiedotteet saatavilla?</label>
               <div>
@@ -165,7 +185,7 @@ const SafetyReview = () => {
           </div>
 
           <div>
-            <label>Räjähdyssuoja-arviointi</label>
+            <h4>Räjähdyssuoja-arviointi</h4>
             <div>
               <label>Käsitelläänkö tilassa kemikaaleja tai pölyjä? Onko räjähdyssuoja-arviointi ajantasalla?</label>
               <div>
@@ -184,7 +204,7 @@ const SafetyReview = () => {
           </div>
 
           <div>
-            <label>Säteilyturvallisuusdokumentit</label>
+            <h4>Säteilyturvallisuusdokumentit</h4>
             <div>
               <label>Onko säteilyturvallisuusohjeet ja luvat ajantasalla?</label>
               <div>
@@ -203,7 +223,7 @@ const SafetyReview = () => {
           </div>
 
           <div>
-            <label>Turvallisuushavainnot</label>
+            <h4>Turvallisuushavainnot</h4>
             <div>
               <label>Onko tilasta tullut turvallisuushavaintoja? Onko korjaavat toimenpiteet tehty?</label>
               <div>
@@ -231,7 +251,6 @@ const SafetyReview = () => {
           <input type="text" name="positiveThings"></input>
         </div>
         <div>
-          <p>VALOKUVAUN LISÄYS TÄHÄN</p>
           <PhotoInput
             type="file"
             accept="image/*"
@@ -241,6 +260,7 @@ const SafetyReview = () => {
         </div>
         </form>
       </GrayBackground>
+      </FormContainer>
     </SafetyReviewContainer>
   );
 };
