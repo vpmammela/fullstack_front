9d6ae74681e813e00a6a417578287ca5a727896c
Marc Bäckman <marcbackman.lapinamk@gmail.com>
2023-12-10 03:20:46 +0200

    all review types can send data to backend

--- src/components/ContinuousReview/ContinuousReview.tsx
+++ src/components/ContinuousReview/ContinuousReview.tsx
@@ -7,6 +7,11 @@ import styled from 'styled-components';
 import { faSmile, faMeh, faFrown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Header from '../header';
+import { useNotification } from '../../NotificationContext';
+import { useReviewContext } from '../../ReviewContext';
+import { createInspectionResult } from '../../services/inspectionresult';
+import { getInspectionTargetById, getInspectionTargetsByEnviromentsId } from '../../services/inspectiontarget';
+import { createInspectionForm } from '../../services/inspectionform';
 
 /*const ContinuousReviewContainer = styled.div`
   position: relative;
@@ -119,16 +124,26 @@ export async function action({ request }: { request: Request }) {
   console.log(result);
 }
 
+interface ResultData {
+  value: number;
+  note: string;
+  title: string;
+  inspectionform_id: number;
+}
+
 // ContinuousReview Component
 const ContinuousReview = () => {
-  const { user } = useUser();
   const [photo, setPhoto] = useState<File | null>(null);
   const [photoUrl, setPhotoUrl] = useState<string | null>(null);
-  const [selectedMood, setSelectedMood] = useState<string>(''); // 'happy', 'neutral', 'sad'
+  const [value, setValue] = useState<number>(0);
+  const [note, setNote] = useState<string>('');
 
-  const handleMoodSelection = (mood: string) => {
-    setSelectedMood(mood);
-  };
+  const { setNotification } = useNotification();
+
+  const { environment_id } = useReviewContext();
+  const { inspectiontarget_id } = useReviewContext();
+
+
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -142,6 +157,113 @@ const ContinuousReview = () => {
     }
   };
 
+  // Function to handle radio button changes
+  const handleRadioChange = (choice: number) => {
+    setValue(choice);
+  };
+
+  // Function to handle text input changes
+  const handleTextChange = (comment: string) => {
+    setNote(comment);
+  };
+
+  // Function to handle data for result
+  const handleDataForResult = (inspectionform_id: number): ResultData => {
+    return {
+      value,
+      note,
+      title: 'Yleisilme',
+      inspectionform_id
+    };
+  };
+
+  // Sends form data and result data to backend
+  const sendResultData = async (inspectiontype: string) => {
+
+    let room: { name: string; } | null = null;
+    try {
+      room = await getInspectionTargetById(inspectiontarget_id!);
+    } catch (e) {
+      setNotification(`Virhe huoneen haussa: ${e}`)
+    }
+    let targets = null;
+    let target = null;
+    try {
+      targets = await getInspectionTargetsByEnviromentsId(environment_id!);
+      target = targets.inspectiontargets.filter((target: { name: string; }) => target.name === room!.name);
+    } catch (e) {
+      setNotification(`Virhe huoneen haussa: ${e}`)
+    }
+
+    const formSend = {
+      environment_id,
+      inspectiontarget_id: target[0].id,
+      inspectiontype
+    }
+    
+    // Upload photo if available
+    if (photo) {
+      const formData = new FormData();
+      formData.append('photo', photo);
+
+      // TODO: Replace with the actual API endpoint.
+      //await axios.post('your-upload-api', formData);
+    }
+
+    // creates form
+    let inspectionform: { id: number; } | null = null;
+    try{
+      inspectionform = await createInspectionForm(formSend);
+    } catch (e) {
+      setNotification(`Virhe katselmoinnin tallentamisessa: ${e}`)
+    }
+
+    // creates result
+    const resultData = handleDataForResult(inspectionform!.id);
+    try {
+      await createInspectionResult(resultData)
+    } catch (e) {
+      setNotification(`Virhe vastauksen tallentamisessa: ${e}`)
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <br></br>
+        <h2>Yleisilme</h2>
+        <label>Puutteellinen</label>
+        <input type="radio" value="1" name="general" onChange={() => handleRadioChange(1)} />
+        <label>Sitoutunut</label>
+        <input type="radio" value="2" name="general" onChange={() => handleRadioChange(2)} />
+        <label>Edelläkävijä</label>
+        <input type="radio" value="3" name="general" onChange={() => handleRadioChange(3)} />
+        <br />
+        <br/>
+        <label>Huomiot: </label>
+        <br/>
+        <input type="text" name="generalComment" onChange={(e) => handleTextChange(e.target.value)} />
+        <br />
+        <br />
+        <h3>Valitse kuva</h3>
+          <FileInput type="file" accept="image/*" capture="environment" onChange={handleFileChange} />
+          <p>
+            Image preview:
+            {photoUrl ? <img src={photoUrl} alt="Selected" style={{ maxWidth: '50%' }} /> : null}
+          </p>
+        <div>
+            <br/>
+            <button type="button" onClick={() => sendResultData("continuous")}>
+              Send Data
+            </button>
+            <br/>
+            <br/>
+        </div>
+      </div>
+    </div>
+  )
+
+/*
   return (
     <div>
         <Header></Header>
@@ -186,8 +308,8 @@ const ContinuousReview = () => {
                 <FormLabel>Valitse kuva</FormLabel>
                 <FileInput type="file" accept="image/*" capture="environment" onChange={handleFileChange} />
                 <p>
-                  {/*Image preview:
-                  {photoUrl ? <img src={photoUrl} alt="Selected" style={{ maxWidth: '50%' }} /> : null}*/}
+                  Image preview:
+                  {photoUrl ? <img src={photoUrl} alt="Selected" style={{ maxWidth: '50%' }} /> : null}
                 </p>
                 <div></div>
               </Form>
@@ -199,6 +321,11 @@ const ContinuousReview = () => {
         </GrayBackground>
     </div>
   );
+*/
 };
 
 export default ContinuousReview;
+function handleDataForResult(category: string, id: number) {
+  throw new Error('Function not implemented.');
+}
+
